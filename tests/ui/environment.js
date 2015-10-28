2f866108bc82c26f0e4025712c03db4912c15bc5
Austin Bingham <austin@sixty-north.com>
2015-10-28 08:53:53 +0100

    disabled chrome extensions to avoid annoying popup

--- tests/ui/environment.js
+++ tests/ui/environment.js
@@ -2,27 +2,30 @@
 var webServerDefaultPort = 6543;
 
 module.exports = {
-  // The address of a running selenium server.
-  seleniumAddress:
+    // The address of a running selenium server.
+    seleniumAddress:
     (process.env.SELENIUM_URL || 'http://localhost:4444/wd/hub'),
 
-  // Capabilities to be passed to the webdriver instance.
-  capabilities: {
-    'browserName':
+    // Capabilities to be passed to the webdriver instance.
+    capabilities: {
+        'browserName':
         (process.env.TEST_BROWSER_NAME || 'chrome'),
-    'version':
-        (process.env.TEST_BROWSER_VERSION || 'ANY')
-  },
+        'version':
+        (process.env.TEST_BROWSER_VERSION || 'ANY'),
+        'chromeOptions': {
+            args: ['--disable-extensions']
+        }
+    },
 
-  // Default http port to host the web server
-  webServerDefaultPort: webServerDefaultPort,
+    // Default http port to host the web server
+    webServerDefaultPort: webServerDefaultPort,
 
-  // Protractor interactive tests
-  interactiveTestPort: 6969,
+    // Protractor interactive tests
+    interactiveTestPort: 6969,
 
-  // A base URL for your application under test.
-  baseUrl:
+    // A base URL for your application under test.
+    baseUrl:
     'http://' + (process.env.HTTP_HOST || 'localhost') +
-          ':' + (process.env.HTTP_PORT || webServerDefaultPort)
+        ':' + (process.env.HTTP_PORT || webServerDefaultPort)
 
 };
