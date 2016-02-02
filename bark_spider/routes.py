c80f9ac63aab9937a41c98f47825099b0e9bff2c
Austin Bingham <austin@sixty-north.com>
2016-02-02 13:01:09 +0000

    Added elm directory to static routing.

--- bark_spider/routes.py
+++ bark_spider/routes.py
@@ -1,5 +1,6 @@
 def configure_routes(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view('elm', 'elm')
     config.add_route('home', '/')
     config.add_route('simulate', '/simulate')
     config.add_route('simulation', '/simulation/{id}')
