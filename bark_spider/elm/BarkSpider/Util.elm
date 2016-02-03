50d0b2f8036bdacd216fe853f8c41c3cc39ebc16
Austin Bingham <austin@sixty-north.com>
2016-02-03 11:33:11 +0000

    Removed some crufty old functions.

--- bark_spider/elm/BarkSpider/Util.elm
+++ bark_spider/elm/BarkSpider/Util.elm
@@ -2,31 +2,5 @@ module BarkSpider.Util where
 
 import Effects exposing (Effects)
 
--- setAt : List a -> Int -> a -> Maybe (List a)
--- setAt l index value =
---   let
---     head = List.take index l
---     tail = List.drop index l |> List.tail
---   in
---     case tail of
---       Nothing ->
---         Nothing
-
---       Just t ->
---         Just (value :: t |> List.append head)
-
--- removeAt : List a -> Int -> List a
--- removeAt l index =
---   let
---     head = List.take index l
---     tail = List.drop index l |> List.tail
---   in
---     case tail of
---       Nothing ->
---         l
-
---       Just t ->
---         List.append head t
-
 noFx : model -> (model, Effects a)
 noFx model = (model, Effects.none)
