2f30b8921bfb64d9781081bf0456269aaacc63c1
Austin Bingham <austin@sixty-north.com>
2016-03-10 17:29:33 +0100

    Embedding sorta working.

--- bark_spider/elm/Main.elm
+++ bark_spider/elm/Main.elm
@@ -1,16 +1,31 @@
 module BarkSpider (..) where
 
 import BarkSpider.App exposing (app)
+import Dict
 import Effects
 import Html
 import Task
+import UrlParameterParser
 
+import Debug
 
 main : Signal Html.Html
 main =
-  app.html
+  let
+    foo = Debug.log "params" parameters
+  in
+    app.html
 
+port locationSearch : String
 
 port tasks : Signal (Task.Task Effects.Never ())
 port tasks =
   app.tasks
+
+parameters : Dict.Dict String String
+parameters =
+  case (UrlParameterParser.parseSearchString locationSearch) of
+    UrlParameterParser.Error _ -> Dict.empty
+    UrlParameterParser.UrlParams dict -> dict
+
+-- TODO: git submodule for urlparam stuff
