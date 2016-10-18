9d310e22510be1b61430ceb0e436350827ccc0d8
Austin Bingham <austin@sixty-north.com>
2016-10-18 14:56:07 +0200

    Coloring of datasets.

--- bark_spider/elm/BarkSpider/Chart.elm
+++ bark_spider/elm/BarkSpider/Chart.elm
@@ -1,23 +1,39 @@
 port module BarkSpider.Chart exposing (..)
 
 import BarkSpider.Model as Model
+import BarkSpider.Util as Util
+
+
+type alias ChartData =
+    { data : Model.SimulationData
+    , name : String
+    , color : ( Int, Int, Int )
+    }
 
 
 plot : List (Result err Model.SimulationResults) -> Cmd msg
 plot results =
     let
-        accum next result =
-            case next of
-                Ok r ->
-                    (r.name, r.data) :: result
-
-                _ ->
-                    result
-
         successes =
-            List.foldl accum [] results
+            List.foldl
+                (\next accum ->
+                    case next of
+                        Ok r ->
+                            r :: accum
+
+                        _ ->
+                            accum
+                )
+                []
+                results
+
+        colored =
+            List.map2
+                (\r color -> { name = r.name, data = r.data, color = color })
+                successes
+                Util.distinctColors
     in
-        render successes
+        render colored
 
 
-port render : List (String, Model.SimulationData) -> Cmd msg
+port render : List ChartData -> Cmd msg
--- bark_spider/elm/BarkSpider/Util.elm
+++ bark_spider/elm/BarkSpider/Util.elm
@@ -1,7 +1,6 @@
 module BarkSpider.Util exposing (distinctColors, noFx)
 
 
-import Color exposing (Color, rgba)
 import Platform.Cmd
 
 
@@ -10,19 +9,20 @@ noFx model =
     ( model, Platform.Cmd.none )
 
 
-{-| A sequence of color templates which are fairly distinct.
+{-| A sequence of RGB color tuples which are fairly distinct.
 
-These are useful for coloring different data sets. We're mimicking the algorithm described here:
+These are useful for coloring different data sets. We're mimicking the algorithm
+described here:
 
     http://stackoverflow.com/questions/309149/generate-distinctly-different-rgb-colors-in-graphs
 -}
-distinctColors : List (Float -> Color)
+distinctColors : List (Int, Int, Int)
 distinctColors =
     let
         colorBases =
             [ 154, 77, 34, 111, 56, 28, 192, 96, 48, 255, 128, 64 ]
 
         colorSet base =
-            [ (rgba 0 0 base), (rgba 0 base 0), (rgba base 0 0), (rgba 0 base base), (rgba base 0 base), (rgba base base 0) ]
+            [ (0, 0, base), (0, base, 0), (base, 0, 0), (0, base, base), (base, 0, base), (base, base, 0) ]
     in
         List.map colorSet colorBases |> List.concat
