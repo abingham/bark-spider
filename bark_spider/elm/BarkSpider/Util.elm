8fe39255aac9fbddd0c73395135320a6b187401b
Austin Bingham <austin@sixty-north.com>
2016-11-27 22:17:21 +0100

    Added lens "tuple" functions.

--- bark_spider/elm/BarkSpider/Util.elm
+++ bark_spider/elm/BarkSpider/Util.elm
@@ -1,4 +1,7 @@
-module BarkSpider.Util exposing (distinctColors)
+module BarkSpider.Util exposing (distinctColors, tuple)
+
+import Monocle.Lens exposing (Lens)
+
 
 {-| A sequence of RGB color tuples which are fairly distinct.
 
@@ -7,8 +10,6 @@ described here:
 
     http://stackoverflow.com/questions/309149/generate-distinctly-different-rgb-colors-in-graphs
 -}
-
-
 distinctColors : List ( Int, Int, Int )
 distinctColors =
     let
@@ -19,3 +20,19 @@ distinctColors =
             [ ( 0, 0, base ), ( 0, base, 0 ), ( base, 0, 0 ), ( 0, base, base ), ( base, 0, base ), ( base, base, 0 ) ]
     in
         List.map colorSet colorBases |> List.concat
+
+
+{-| Tuple `Lens a b` with `Lens a c` and returns `Lens a (b,c)`
+
+Borrowed from monocle. We should use its version once we can update to 0.18.
+-}
+tuple : Lens a b -> Lens a c -> Lens a ( b, c )
+tuple left right =
+    let
+        get a =
+            ( left.get a, right.get a )
+
+        set ( b, c ) a =
+            right.set c (left.set b a)
+    in
+        Lens get set
